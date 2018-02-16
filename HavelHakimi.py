baab1e1be84bc533c091688a2032bccd85b1cc83
jordanhewko@gmail.com <hewkoj2@mymacewan.ca>
2018-02-15 20:22:24 -0700

    change command line argument to be a sequence, checks for Eulerian

--- HavelHakimi.py
+++ HavelHakimi.py
@@ -3,17 +3,16 @@ import sys
 def get_largest_element(sequence):
 
 	largest = sequence[len(sequence) - 1]
-	print("largest element is " + str(largest))
 
 	return int(largest)
 
 def remove_last_element(sequence):
-	
+
 	del sequence[-1]
 	return sequence
 
 def reduce(sequence):
-	
+
 	largest = get_largest_element(sequence)
 	sequence = remove_last_element(sequence)
 	last_element = len(sequence)
@@ -25,31 +24,31 @@ def reduce(sequence):
 		sequence[last_element - i] -= 1
 
 	sequence.sort()
-	print_sequence(sequence)
+
 	return sequence
 
 def print_sequence(sequence):
-	
+
 	print("sequence: [", end='')
-	
+
 	for i in range(0, len(sequence)):
-		
+
 		if i != (len(sequence) - 1):
 			print(str(sequence[i]) + ", ", end='')
-		
+
 		else:
 			print(str(sequence[i]) , end='')
 
 	print("]")
 
 def get_valid_integer(i):
-	
+
 	checker = False
 
 	while(checker == False):
-		
+
 		number = input("enter digit number " + str(i) + ": ") 
-		
+
 		try:
 			int(number)
 			checker = True
@@ -57,16 +56,14 @@ def get_valid_integer(i):
 		except ValueError:
 			print("digit must be a number, try again.")
 
-	return number		
+	return number
 
-def build_sequence(size, end=''):
+def build_sequence(in_string, end=''):
 
-	sequence = []
+	sequence = in_string.split(",")
 
-	for i in range(0,size):
-		
-		number = get_valid_integer(i) 
-		sequence.append(int(number))
+	for i in range(0,len(sequence)):
+		sequence[i] = int(sequence[i])
 
 	sequence.sort()
 
@@ -84,7 +81,7 @@ def check_handshake_lemma(sequence):
 	count = 0
 	for i in sequence:
 		if (is_odd(i) == True):
-			count += 1 
+			count += 1
 
 	if is_odd(count):
 		return False
@@ -95,39 +92,61 @@ def check_handshake_lemma(sequence):
 def handle_opt(argv):
 
 	if len(argv) != 2:
-		print("usage: python3 HavelHakimi.py sequence_length")
+		print("usage: python3 HavelHakimi.py sequence")
 		sys.exit(1)
-	
+
 	else:
 
 		try:
-			out_value = int(argv[1])
-			return argv[1]
+			out_value = argv[1]
+			return out_value
 
 		except ValueError:
-			
-			print("Type Error: sequence_length must be an integer")
+
+			print("ERROR")
 			sys.exit(1)
 
+def check_sequence(sequence):
 
-def main():
 
-	size = handle_opt(sys.argv)
-	sequence = build_sequence(int(size))
-	
-	print_sequence(sequence)
 	if (check_handshake_lemma(sequence) == False):
 			print("Sequence is not a graph (handshake lemma)")
-			sys.exit(1)
+			return False
 
 	while len(sequence) > 1:
 		is_reducable = reduce(sequence)
-	
+
 		if is_reducable == False:
 			print("Sequence is not a graph ")
-			sys.exit(1)
+			return False
 	else:
 		print("Sequence is a graph")
-		sys.exit(0)
+		return True
 
-main()
+def check_euler(sequence):
+
+	odd_numbers = 0
+	for i in range(0, len(sequence)):
+
+		if (sequence[i] % 2) != 0:
+			odd_numbers += 1
+
+	if odd_numbers == 2:
+		print("Sequence is semi-Eulerian")
+	elif odd_numbers > 2:
+		print("Sequence is neither Eulerian or semi-Eulerian")
+	elif odd_numbers == 0:
+		print("Graph is Eulerian")
+
+	return
+
+def main():
+
+	sequence = handle_opt(sys.argv)
+	sequence = build_sequence(sequence)
+	result = check_sequence(sequence)
+	if result == True:
+		check_euler(sequence)
+
+if __name__ == "__main__":
+    main()
